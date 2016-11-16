f792fe1d9e58b4c5620098ae60e157299f42e07f
Junseong Hong <jakehong0521@berkeley.edu>
2016-11-15 19:13:00 -0800

    update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController 
+	
 	def respond
 	end
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@users = User.find(params[:email])
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	devise :database_authenticatable, :registerable,
+	:recoverable, :rememberable, :trackable, :validatable
+	has_many :pets
+	has_many :requests
 end
