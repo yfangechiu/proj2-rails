09d8eb0cb0c1ade9ff79fd9179de974697696db2
Takara Satone <takarasatone@gmail.com>
2016-11-16 14:48:43 -0800

    finished check in reqs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+%w(Jake).each do |name|
+	User.create name: name, id: 1
+end
